521fd7235efc3a1a3b18361f40e6a63c768bd21c
Dustin Morrill <dmorrill10@gmail.com>
2015-07-19 06:58:58 -0600

    Add DataStore::Store#back

--- src/lib/data_store.h
+++ src/lib/data_store.h
@@ -14,21 +14,24 @@ template <class T>
 class Store {
  public:
   Store(uint blockSize, uint initalNumElements = 1e1)
-      : data_(initalNumElements * blockSize), base_(0), blockSize_(blockSize) {}
+      : data_(initalNumElements * blockSize),
+        base_(0),
+        top_(0),
+        blockSize_(blockSize) {}
   virtual ~Store() {}
 
   void push(const T* newData) {
-    const auto top = base_ + blockSize_;
-    ensureLargeEnough(top);
+    base_ = top_;
+    top_ += blockSize_;
+    ensureLargeEnough();
     std::copy(newData, newData + blockSize_, data_.begin() + base_);
-    base_ = top;
   }
 
   void push(size_t numElements) {
-    const auto top = base_ + blockSize_;
-    ensureLargeEnough(top);
+    base_ = top_;
+    top_ += blockSize_;
+    ensureLargeEnough();
     std::fill(&data_[base_], &data_[top], 0.0);
-    base_ = top;
   }
 
   void clear() {
@@ -38,6 +41,8 @@ class Store {
   T* data(uint blockIndex) { return &data_[baseIndex(blockIndex)]; }
   const T* data(uint blockIndex) const { return &data_[baseIndex(blockIndex)]; }
 
+  T* back() { return data(base_); }
+
   uint blockSize() const { return blockSize_; }
 
   uint baseIndex(uint blockIndex) const { return blockIndex * blockSize_; }
@@ -45,12 +50,12 @@ class Store {
   uint size() const { return base_ / blockSize_; }
 
  protected:
-  void ensureLargeEnough(uint top) {
-    if (data_.size() <= top) {
+  void ensureLargeEnough() {
+    if (data_.size() <= top_) {
       const auto& dataEnd = data_.end();
 
       data_.insert(dataEnd,
-                   std::max(uint(2 * data_.size()), uint(top)) - data_.size(),
+                   std::max(uint(2 * data_.size()), uint(top_)) - data_.size(),
                    0.0);
     }
   }
@@ -58,6 +63,7 @@ class Store {
  protected:
   std::vector<T> data_;
   uint base_;
+  uint top_;
   const uint blockSize_;
 };
 }
